629a44f1f47ad4ec90e11a59e1f5e354672c8d2f
vinhhoang-1312 <tranphanvinhhoang7@gmail.com>
2021-06-11 11:38:18 +0700

    Done Create course

--- StaffTrainee/Controllers/CoursesController.cs
+++ StaffTrainee/Controllers/CoursesController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Ajax.Utilities;
 using StaffTrainee.Models;
-using StaffTrainee.ViewModel;
+using StaffTrainee.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,77 +9,77 @@ using System.Web.Mvc;
 
 namespace StaffTrainee.Controllers
 {
-	public class CoursesController : Controller
-	{
-		private ApplicationDbContext _context;
+    public class CoursesController : Controller
+    {
+        private ApplicationDbContext _context;
 
-		public CoursesController()
-		{
-			_context = new ApplicationDbContext();
-		}
-		// GET: Course
+        public CoursesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Course
 
-		[HttpGet]
-		public ActionResult Index(string searchString)
-		{
-
-		   
-			//var userId = User.Identity.GetUserId();
-
-			var course = _context.Courses
-				//.Include(t => t.Category)
-				//.Where(t => t.UserId.Equals(userId))
-				.ToList();
+        [HttpGet]
+        public ActionResult Index(string searchString)
+        {
 
 
+            //var userId = User.Identity.GetUserId();
 
-			if (!searchString.IsNullOrWhiteSpace())
-			{
-				course = _context.Courses.Where(t => t.Description.Contains(searchString)).ToList();
-			}
-
-			return View(course);
-		}
-		[HttpGet]
-		public ActionResult Create()
-		{
-			var viewModel = new CourseCategoriesViewModel()
-			{
-				Categories = _context.Categories.ToList()
-			};
-
-			return View(viewModel);
-		}
-
-		[HttpPost]
-		public ActionResult Create(Course course)
-		{
-			if (!ModelState.IsValid)
-			{
-				var viewModel = new CourseCategoriesViewModel()
-				{
-					Course = course,
-					Categories = _context.Categories.ToList()
-				};
-
-				return View(viewModel);
-			}
-
-			//var userId = User.Identity.GetUserId();
-			var newTodo = new Course()
-			{
-				Description = course.Description,
-				CategoryId = course.CategoryId,
-				Name = course.Name,
-				
-			};
-
-			_context.Courses.Add(newTodo);
-			_context.SaveChanges();
-
-			return RedirectToAction("Index");
-		}
+            var course = _context.Courses
+                //.Include(t => t.Category)
+                //.Where(t => t.UserId.Equals(userId))
+                .ToList();
 
 
-	}
+
+            if (!searchString.IsNullOrWhiteSpace())
+            {
+                course = _context.Courses.Where(t => t.Description.Contains(searchString)).ToList();
+            }
+
+            return View(course);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseCategoriesViewModel()
+            {
+                Categories = _context.Categories.ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModels = new CourseCategoriesViewModel()
+                {
+                    Course = course,
+                    Categories = _context.Categories.ToList()
+                };
+
+                return View(viewModels);
+            }
+
+            //var userId = User.Identity.GetUserId();
+            var newTodo = new Course()
+            {
+                Description = course.Description,
+                CategoryId = course.CategoryId,
+                Name = course.Name,
+
+            };
+
+            _context.Courses.Add(newTodo);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
 }
--- StaffTrainee/ViewModels/CourseCategoriesViewModel.cs
+++ StaffTrainee/ViewModels/CourseCategoriesViewModel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace StaffTrainee.ViewModel
+namespace StaffTrainee.ViewModels
 {
     public class CourseCategoriesViewModel
     {
